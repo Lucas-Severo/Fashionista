02d76dbb5124b48f6bb08f608c0b0a0582cec8ae
Lucas-Severo <lucas_severo@outlook.com.br>
2020-04-11 11:59:36 -0400

    Add items to the cart component

--- src/components/Cart/index.js
+++ src/components/Cart/index.js
@@ -5,6 +5,8 @@ import './style.css';
 import { faArrowLeft } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
+import image3 from '../../pages/Home/assets/image3.jpg';
+
 export default function Cart() {
     function hideCart() {
         document.querySelector(".cart").classList.remove("cart--visible");
@@ -21,8 +23,63 @@ export default function Cart() {
                     <p className="cart__title">Sacola</p>
                 </div>
             </header>
-            <main className="cart__content">
+            <main className="cart__list">
+                <div className="cart__item item">
+                    <img src={image3} alt="" className="item__image"/>
+                    <p className="item__title">Blazzer Cinza</p>
+                    <p className="item__size">Tam: P</p>
+                    <p className="item__price">R$48,50</p>
+                    <p className="item__installments">2x de R$24,50</p>
+                    <p className="item__remove">Remover Item</p>
 
+                    <div className="item__info">
+                        <button className="item__decreases">-</button>
+                        <p className="item__amount">0</p>
+                        <button className="item__increases">+</button>
+                    </div>
+                </div>
+                <div className="cart__item item">
+                    <img src={image3} alt="" className="item__image"/>
+                    <p className="item__title">Blazzer Cinza</p>
+                    <p className="item__size">Tam: P</p>
+                    <p className="item__price">R$48,50</p>
+                    <p className="item__installments">2x de R$24,50</p>
+                    <p className="item__remove">Remover Item</p>
+
+                    <div className="item__info">
+                        <button className="item__decreases">-</button>
+                        <p className="item__amount">0</p>
+                        <button className="item__increases">+</button>
+                    </div>
+                </div>
+                <div className="cart__item item">
+                    <img src={image3} alt="" className="item__image"/>
+                    <p className="item__title">Blazzer Cinza</p>
+                    <p className="item__size">Tam: P</p>
+                    <p className="item__price">R$48,50</p>
+                    <p className="item__installments">2x de R$24,50</p>
+                    <p className="item__remove">Remover Item</p>
+
+                    <div className="item__info">
+                        <button className="item__decreases">-</button>
+                        <p className="item__amount">0</p>
+                        <button className="item__increases">+</button>
+                    </div>
+                </div>
+                <div className="cart__item item">
+                    <img src={image3} alt="" className="item__image"/>
+                    <p className="item__title">Blazzer Cinza</p>
+                    <p className="item__size">Tam: P</p>
+                    <p className="item__price">R$48,50</p>
+                    <p className="item__installments">2x de R$24,50</p>
+                    <p className="item__remove">Remover Item</p>
+
+                    <div className="item__info">
+                        <button className="item__decreases">-</button>
+                        <p className="item__amount">0</p>
+                        <button className="item__increases">+</button>
+                    </div>
+                </div>
             </main>
         </div>
     );
