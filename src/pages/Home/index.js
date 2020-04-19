bfa9a4c13049dd86045112bc8a8bde8d6258f617
Lucas-Severo <lucas_severo@outlook.com.br>
2020-04-19 09:20:34 -0400

    Add the discount percentage to products on sale

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -33,14 +33,44 @@ export default function Home() {
                 {
                     products.map(product => (
                         <div key={product.code_color} className="clothes__item">
-                        <Link to={"/product/"+product.code_color}>
-                            { product.image 
-                                ? <img src={product.image} className="clothes__image" alt={product.name}/>
-                                : <img src={notFoundImage} className="clothes__image" alt={product.name}/>
-                            }
-                        </Link>
-                        <Link className="clothes__title" to={"/product/"+product.code_color}>{product.name}</Link>
-                        <p className="clothes__price">{product.actual_price}</p>
+                        
+                        {product.on_sale 
+                            ? (
+                            <>
+                                <Link to={"/product/"+product.code_color}>
+                                <div className="clothes__image-container">
+                                    { product.image 
+                                        ? <img src={product.image} className="clothes__image" alt={product.name}/>
+                                        : <img src={notFoundImage} className="clothes__image" alt={product.name}/>
+                                    }
+                                    <div className="clothes__off">{product.discount_percentage}</div>
+                                </div>
+                                </Link>
+
+                                <Link className="clothes__title" to={"/product/"+product.code_color}>{product.name}</Link>
+
+                                <div className="clothes__prices">
+                                    <p className="clothes__price--regular">{product.regular_price}</p>
+                                    <p className="clothes__price">{product.actual_price}</p>
+                                </div>
+                            </>
+                        ) : (
+                            <>
+                            <Link to={"/product/"+product.code_color}>
+                            <div className="clothes__image-container">
+                                { product.image 
+                                    ? <img src={product.image} className="clothes__image" alt={product.name}/>
+                                    : <img src={notFoundImage} className="clothes__image" alt={product.name}/>
+                                }
+                            </div>
+                            </Link>
+
+                            <Link className="clothes__title" to={"/product/"+product.code_color}>{product.name}</Link>
+
+                            <p className="clothes__price">{product.actual_price}</p>
+                            </>
+                        )}
+                        
                         </div>
                     ))
                 }
