f616d94a83664b59ce7385ede8b4a60e16de8e87
Lucas-Severo <lucas_severo@outlook.com.br>
2020-04-26 21:28:38 -0400

    Add product quantity to the cart component

--- src/components/Cart/index.js
+++ src/components/Cart/index.js
@@ -1,15 +1,55 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 import { connect } from 'react-redux';
+
+import * as actionCreator from '../../redux/actions';
 
 import './style.css';
 
 import { faArrowLeft } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
-function Cart({cartProducts}) {
+function Cart({cartProducts, setCartProducts}) {
     function hideCart() {
         document.querySelector(".cart").classList.remove("cart--visible");
         document.querySelector("body").classList.remove("app--scroll-lock");
+    }
+
+    function decreases(id, size) {
+        const item = cartProducts.filter(product => {
+            if(product.code_color === id &&
+                product.size === size) {
+                product.qtd -= 1;
+                return product;
+            }
+            return product
+        });
+
+        setCartProducts(item);
+    }
+
+    function increases(id, size) {
+        const item = cartProducts.filter(product => {
+            if(product.code_color === id &&
+                product.size === size) {
+                product.qtd += 1;
+                return product;
+            }
+            return product;
+        });
+
+        setCartProducts(item);
+    }
+
+    function removeItem(id, size) {
+        const items = cartProducts.filter(product => {
+            if(!((product.code_color === id) && (product.size === size))) {
+                return product;
+            }
+            return null;
+        });
+
+        setCartProducts(items);
     }
 
     return (
@@ -27,21 +67,27 @@ function Cart({cartProducts}) {
                     cartProducts.map(cartProduct => (
                     <div className="cart__item item" key={cartProduct.code_color}>
                     <img src={cartProduct.image} alt="" className="item__image"/>
-                        <p className="item__title">{cartProduct.name}</p>
+                        <Link 
+                            to={`/product/${cartProduct.code_color}`} 
+                            className="item__title"
+                            onClick={hideCart}>{cartProduct.name}</Link>
                         <p className="item__size">Size: {cartProduct.size}</p>
                         <p className="item__price">{cartProduct.actual_price}</p>
                         <p className="item__installments">{cartProduct.installments}</p>
-                        <p className="item__remove">Remover Item</p>
+                        <p className="item__remove" onClick={() => removeItem(cartProduct.code_color, cartProduct.size)}>Remover Item</p>
 
                         <div className="item__info">
-                            <button className="item__decreases">-</button>
+                            <button disabled={cartProduct.qtd === 1} className="item__decreases" onClick={() => decreases(cartProduct.code_color, cartProduct.size)}>-</button>
                             <p className="item__amount">{cartProduct.qtd}</p>
-                            <button className="item__increases">+</button>
+                            <button className="item__increases" onClick={() => increases(cartProduct.code_color, cartProduct.size)}>+</button>
                         </div>
                     </div>
                     ))
                 }
             </main>
+            <footer className="cart__price">
+                300reais
+            </footer>
         </div>
     );
 }
@@ -50,4 +96,4 @@ const mapStateToProps = state => ({
   cartProducts: state.cartProducts
 });
 
-export default connect(mapStateToProps)(Cart);
+export default connect(mapStateToProps, actionCreator)(Cart);
