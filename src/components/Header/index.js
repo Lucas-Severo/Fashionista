841227da058b69f6c0e29e07508c992dfb3e7e37
Lucas Rodrigues Severo <lucas_severo@outlook.com.br>
2020-05-01 20:34:34 -0400

    Fixed button to hide the cart/search component

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -8,14 +8,30 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 function Header({productsAmount}) {
 
+    function hide(class1, class2) {
+        // target div
+        const appDiv = document.querySelector(".app");
+
+        document.querySelector(".app--scroll-lock").addEventListener("click", (e) => {
+            if(e.target === appDiv) {
+                document.querySelector(class1).classList.remove(class2);
+                document.querySelector(".app").classList.remove("app--scroll-lock");
+            }
+        });
+    }
+
     function showCart() {
-        document.querySelector(".cart").classList.toggle("cart--visible");
+        document.querySelector(".cart").classList.add("cart--visible");
         document.querySelector(".app").classList.add("app--scroll-lock");
+
+        hide(".cart", "cart--visible");
     }
 
     function showSearch() {
         document.querySelector(".search").classList.toggle("search--visible");
         document.querySelector(".app").classList.add("app--scroll-lock");
+
+        hide(".search", "search--visible");
     }
 
     return (
